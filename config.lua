282e1047ad774ab9dc7a85df6117dc11131ad105
RetryR1v2 <147660115+RetryR1v2@users.noreply.github.com>
2023-12-13 18:44:13 +0100

    Initial Release

---
+++ config.lua
@@ -0,0 +1,6 @@
+Config = {}
+
+
+
+Config.RagdollKey = 'Z'  -----Ragdoll Key
+Config.RagdollTime = 3000  ---- 3000 = 3 Sec
---
+++ server/server.lua
@@ -0,0 +1,43 @@
+local RSGCore = exports['rsg-core']:GetCoreObject()
+
+
+-----------------------------------------------------------------------
+-- version checker
+-----------------------------------------------------------------------
+local function versionCheckPrint(_type, log)
+    local color = _type == 'success' and '^2' or '^1'
+
+    print(('^5['..GetCurrentResourceName()..']%s %s^7'):format(color, log))
+end
+
+local function CheckVersion()
+    PerformHttpRequest('https://raw.githubusercontent.com/RetryR1v2/mms-ragdoll/main/version.txt', function(err, text, headers)
+        local currentVersion = GetResourceMetadata(GetCurrentResourceName(), 'version')
+
+        if not text then 
+            versionCheckPrint('error', 'Currently unable to run a version check.')
+            return 
+        end
+
+      
+        if text == currentVersion then
+            versionCheckPrint('success', 'You are running the latest version.')
+        else
+            versionCheckPrint('error', ('Current Version: %s'):format(currentVersion))
+            versionCheckPrint('success', ('Latest Version: %s'):format(text))
+            versionCheckPrint('error', ('You are currently running an outdated version, please update to version %s'):format(text))
+        end
+    end)
+end
+
+
+
+
+
+
+
+
+--------------------------------------------------------------------------------------------------
+-- start version check
+--------------------------------------------------------------------------------------------------
+CheckVersion()
